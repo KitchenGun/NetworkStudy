be0f8d1df11b5e3f011953ad58fb4082c3621de0
KitchenGun <kitchengun124@gmail.com>
2020-04-09 21:45:39 +0900

    UnityFile 추가

--- HelloWorld_udp_server/Hello_world_udp_server/Program.cs
+++ HelloWorld_udp_server/Hello_world_udp_server/Program.cs
@@ -14,17 +14,18 @@ namespace Hello_world_udp_server
     {
         private static IPAddress GroupAddress = IPAddress.Parse("224.0.0.1");
         private static int GroupPort = 1004;
-        static void Main(string[] args)
+        static void Main(string[] args)//엔트리 포인트 함수의 이름
         {
             try
             {
                 string message = "hello";//출력할 메세지
-                UdpClient sender = new UdpClient();
+
+                UdpClient sender = new UdpClient();//udp클라이언트 인스턴스와 브로드캐스팅 네트워크 연결
                 sender.JoinMulticastGroup(GroupAddress);
 
-                IPEndPoint groupEP = new IPEndPoint(GroupAddress, GroupPort);
+                IPEndPoint groupEP = new IPEndPoint(GroupAddress, GroupPort);//네트워크 단말을 지정
 
-                byte[] bytes = new byte[1024];//버퍼
+                byte[] bytes = new byte[1024];//버퍼 바이트
 
                 int i = 0;
                 while(i<100)//메세지 전송횟수
@@ -36,12 +37,12 @@ namespace Hello_world_udp_server
                     bytes = null;//바이트로 변환
                     bytes = Encoding.UTF8.GetBytes(msg);
 
-                    sender.Send(bytes, bytes.Length, groupEP);//전송
+                    sender.Send(bytes, bytes.Length, groupEP);//sender인스턴스를 통해 단말로 전송 후 카운트 증가
 
                 }
                 sender.Close();
             }
-            catch (SocketException ex)
+            catch (SocketException ex)//아래내용 출력과 입력 대기 
             {
                 Console.WriteLine(ex);
                 Console.ReadLine();
---
+++ Unity_NetWork/Assets/Scripts/Launcher.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    private string gameVersion = "1";
+
+
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;//자동 씬 동기화 true
+    }
+    private void Start()
+    {
+        Connect();
+    }
+
+    public void Connect()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            PhotonNetwork.GameVersion = gameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {//연결
+        Debug.Log("마스터연결");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {//연결해제
+        Debug.LogWarningFormat("연결 해제 {0}", cause);
+    }
+
+    public override void OnJoinedRoom()
+    {//방에 입장
+        Debug.Log("방에 합류했습니다.");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {//랜덤방을 못 찾을 경우
+        Debug.Log("랜덤방이 없음");
+        PhotonNetwork.CreateRoom(null, new RoomOptions());
+    }
+}
